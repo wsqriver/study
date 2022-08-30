4fd12bc04c706daefeb4fe064ac807aacdede3eb
wsqriver <531153213@qq.com>
2022-08-30 14:57:42 +0800

    docker file

--- SpringBootDemo/src/main/java/com/controller/SpringBootController.java
+++ SpringBootDemo/src/main/java/com/controller/SpringBootController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SpringBootController {
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello(){
         return "hello";
     }
